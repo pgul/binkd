930352af8d50a77d29b53302aba9bc8e340e579a
Pavel Gulchouck <gul@gul.kiev.ua>
2002-05-11 10:28:11 +0000

    fix spelling

--- protocol.c
+++ protocol.c
@@ -15,6 +15,9 @@
  * $Id$
  *
  * $Log$
+ * Revision 2.14  2002/05/11 10:28:11  gul
+ * fix spelling
+ *
  * Revision 2.13  2002/05/11 10:27:22  gul
  * Do not send empty (60-bytes) pkt-files
  *
@@ -1037,7 +1040,7 @@ static int OK (STATE *state, char *buf, int sz)
 {
   state->state = !strcmp (state->to->pwd, "-") ? P_NONSECURE : P_SECURE;
   if (state->state == P_SECURE && strcmp(buf, "non-secure") == 0)
-    Log (1, "Warning: remote set UNSECURE sesion");
+    Log (1, "Warning: remote set UNSECURE session");
   complete_login (state);
   return 1;
 }
