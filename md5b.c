e83817391d2f6ca93bc47b8573db39133d770a26
Pavel Gulchouck <gul@gul.kiev.ua>
2003-03-02 08:08:49 +0000

    Added CVS ID and Revision

--- md5b.c
+++ md5b.c
@@ -23,6 +23,15 @@ These notices must be retained in any copies of any part of this
 documentation and/or software.
  */
 
+/*
+ * $Id$
+ *
+ * $Log$
+ * Revision 2.3  2003/03/02 08:08:49  gul
+ * Added CVS ID and Revision
+ *
+ */
+
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
