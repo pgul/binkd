25675d461e514be57202b12a41645a7df5caa694
Pavel Gulchouck <gul@gul.kiev.ua>
2003-06-11 13:27:24 +0000

    Auto increase patchlevel

--- Config.h
+++ Config.h
@@ -1,4 +1,4 @@
-/*
+1*
  *  Config.h -- misc defines
  *
  *  Config.h is a part of binkd project
@@ -15,6 +15,9 @@
  * $Id$
  *
  * $Log$
+ * Revision 2.10.2.2  2003/06/11 13:27:24  gul
+ * Auto increase patchlevel
+ *
  * Revision 2.10.2.1  2003/06/06 16:17:38  gul
  * Change version
  *
