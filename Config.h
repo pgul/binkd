ee1a3bf17c07300031242c0a3e4281496af38cc6
Stas Degteff <grumbler@grumbler.org>
2003-06-24 13:46:35 +0000

    Auto increase patchlevel

--- Config.h
+++ Config.h
@@ -15,6 +15,9 @@
  * $Id$
  *
  * $Log$
+ * Revision 2.34  2003/06/24 13:46:35  stas
+ * Auto increase patchlevel
+ *
  * Revision 2.33  2003/06/24 08:08:49  stas
  * Auto increase patchlevel
  *
@@ -154,7 +157,7 @@
 #define PRTCLNAME "binkp"
 #define PRTCLVER "1.1"
 #define MYNAME "binkd"
-#define MYVER "0.9.6a-22"
+#define MYVER "0.9.6a-23"
 
 #define PRODCODE 0x13FF  /* by FTSCPROD */
 
