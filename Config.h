280282e8979e42e3078dfaac5d843af13ea30cf5
Pavel Gulchouck <gul@gul.kiev.ua>
2003-07-06 06:48:28 +0000

    Auto increase patchlevel

--- Config.h
+++ Config.h
@@ -15,6 +15,9 @@
  * $Id$
  *
  * $Log$
+ * Revision 2.47  2003/07/06 06:48:28  gul
+ * Auto increase patchlevel
+ *
  * Revision 2.46  2003/07/04 08:13:20  val
  * Auto increase patchlevel
  *
@@ -193,7 +196,7 @@
 #define PRTCLNAME "binkp"
 #define PRTCLVER "1.1"
 #define MYNAME "binkd"
-#define MYVER "0.9.6a-35"
+#define MYVER "0.9.6a-36"
 
 #define PRODCODE 0x13FF  /* by FTSCPROD */
 
