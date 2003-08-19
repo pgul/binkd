8a04ae7abee2cc7ba4f3c787d7433d2a842098a0
Pavel Gulchouck <gul@gul.kiev.ua>
2003-08-19 18:08:08 +0000

    Avoid double exitfunc() call

--- nt/breaksig.c
+++ nt/breaksig.c
@@ -24,6 +24,9 @@
  *
  * Revision history:
  * $Log$
+ * Revision 2.7  2003/08/19 18:08:08  gul
+ * Avoid double exitfunc() call
+ *
  * Revision 2.6  2003/08/05 05:36:14  hbrew
  * 'static const char rcsid[]' removed
  *
@@ -131,10 +134,6 @@ BOOL SigHandler(DWORD SigType) {
          Log(1,"Interrupted by unknown signal");
          break;
    }
-   exitfunc();
-#ifdef BINKDW9X
-   ExitProcess(0); /* Sometime binkd9x exit incorrectly (may be others threads still work) */
-#endif
    return (FALSE);
 }
 
