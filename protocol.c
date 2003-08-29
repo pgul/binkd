6e4930fc0f337b588d15678d90bb2037a896dacc
Pavel Gulchouck <gul@gul.kiev.ua>
2003-08-29 13:27:34 +0000

    Do not save zero-length .dt files

--- protocol.c
+++ protocol.c
@@ -15,6 +15,9 @@
  * $Id$
  *
  * $Log$
+ * Revision 2.111  2003/08/29 13:27:34  gul
+ * Do not save zero-length .dt files
+ *
  * Revision 2.110  2003/08/26 22:18:48  gul
  * Fix compilation under w32-mingw and os2-emx
  *
@@ -505,16 +508,16 @@ static int init_protocol (STATE *state, SOCKET socket, FTN_NODE *to, BINKD_CONFI
   return 1;
 }
 
-/*
- * Clears protocol buffers and queues, closes files, etc.
+/* 
+ * Close file currently receiving,
+ * remove .hr and .dt if it's partial pkt or zero-length
  */
-static int deinit_protocol (STATE *state, BINKD_CONFIG *config)
+static int close_partial (STATE *state, BINKD_CONFIG *config)
 {
-  int i;
+  off_t s;
 
   if (state->in.f)
   {
-    off_t s;
     if (ispkt (state->in.netname))
     {
       Log (2, "%s: partial .pkt", state->in.netname);
@@ -523,13 +526,25 @@ static int deinit_protocol (STATE *state, BINKD_CONFIG *config)
     else
     {
       if ((s = ftell (state->in.f)) == 0)
-	Log (4, "%s: empty partial", state->in.netname);
+      Log (4, "%s: empty partial", state->in.netname);
     }
     fclose (state->in.f);
     if (s == 0)
       inb_reject (state->in.netname, state->in.size, state->in.time,
-		  state->fa, state->nallfa, state->inbound, config);
+                  state->fa, state->nallfa, state->inbound, config);
   }
+  TF_ZERO (&state->in);
+  return 0;
+}
+
+/*
+ * Clears protocol buffers and queues, closes files, etc.
+ */
+static int deinit_protocol (STATE *state, BINKD_CONFIG *config)
+{
+  int i;
+
+  close_partial(state, config);
   if (state->out.f)
     fclose (state->out.f);
   if (state->flo.f)
@@ -1701,9 +1716,8 @@ static int start_file_recv (STATE *state, char *args, int sz, BINKD_CONFIG *conf
 	strcmp (argv[0], state->in.netname))	/* ...a file with another *
 						 * name! */
     {
-      fclose (state->in.f);
       Log (1, "receiving of %s interrupted", state->in.netname);
-      TF_ZERO (&state->in);
+      close_partial (state, config);
     }
     if ((state->ND_flag & THEY_ND) && state->in_complete.netname[0])
     { /* rename complete received file to its true form */
