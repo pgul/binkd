fd0a194059dc3f19c9dee74f439183aa412148d5
Alexander Reznikov <homebrewer@yandex.ru>
2003-09-12 09:57:40 +0000

    Close process and thread handles after CreateProcess()

--- nt/win9x.c
+++ nt/win9x.c
@@ -16,6 +16,9 @@
  *
  * Revision history:
  * $Log$
+ * Revision 2.4.2.6  2003/09/12 09:57:40  hbrew
+ * Close process and thread handles after CreateProcess()
+ *
  * Revision 2.4.2.5  2003/09/12 09:44:57  hbrew
  * Fix old noncritical bug in binkd9x (STD_OUTPUT_HANDLE --> STD_INPUT_HANDLE)
  *
@@ -136,11 +139,18 @@ int win9xExec(char *cmdline)
 {
   STARTUPINFO si;
   PROCESS_INFORMATION pi;
+  BOOL rc;
 
   memset(&si, 0, sizeof(si));
   si.cb=sizeof(si);
 
-  return CreateProcess(NULL, cmdline, NULL, NULL, 0, CREATE_DEFAULT_ERROR_MODE, NULL, NULL, &si, &pi);
+  if ((rc = CreateProcess(NULL, cmdline, NULL, NULL, 0, CREATE_DEFAULT_ERROR_MODE, NULL, NULL, &si, &pi)))
+  {
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+  }
+
+  return rc;
 }
 
 void win9xAtExit(void)
