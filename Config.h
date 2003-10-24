f6b2f1bd75eaae83c6cb519bfcf05e1e325e028c
val khokhlov <val@vk.kiev.ua>
2003-10-24 06:38:44 +0000

    Auto increase patchlevel, set 1.0a-273
    fix warning

--- Config.h
+++ Config.h
@@ -55,7 +55,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 272
+#define PATCHLEVEL 273
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
