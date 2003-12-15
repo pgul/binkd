73e7505c6abcb2c56d20eed73c5674d582909f29
Pavel Gulchouck <gul@gul.kiev.ua>
2003-12-15 00:03:58 +0000

    Auto increase patchlevel, set 1.0a-309 NZ option for M_GET command - request uncompressed file

--- Config.h
+++ Config.h
@@ -55,7 +55,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 308
+#define PATCHLEVEL 309
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
