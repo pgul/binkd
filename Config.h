3f0c943781cdc92c44fdd094d6ebc3a0fde10319
val khokhlov <val@vk.kiev.ua>
2004-01-08 12:46:13 +0000

    Auto increase patchlevel, set 1.0a-359 correct macrodef for msvc (_msc_ver instead of __msc__)

--- Config.h
+++ Config.h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 358
+#define PATCHLEVEL 359
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
