c4aa726c0ab50f496a5c58c924b533ec3e53501d
val khokhlov <val@vk.kiev.ua>
2004-11-21 11:55:45 +0000

    Auto increase patchlevel, set 1.0a-435 remove old commented code for limit-rate debug output

--- Config.h
+++ Config.h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 434
+#define PATCHLEVEL 435
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
