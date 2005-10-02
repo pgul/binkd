f7e4dfe5247c56225136d4617f90946c4f3b4b53
Pavel Gulchouck <gul@gul.kiev.ua>
2005-10-02 16:59:11 +0000

    Avoid conflicts if getopt() exists but HAVE_GETOPT not defined

--- getopt.h
+++ getopt.h
@@ -7,6 +7,13 @@
       01Oct89 Add function prototype for getopt                      ahd
   */
 
+/* avoid conflicts if getopt() exists but HAVE_GETOPT not defined */
+#define getopt(argc, argv, opts)	binkd_getopt(argc, argv, opts)
+#define init_getopt			binkd_init_getopt
+#define optind				binkd_optind
+#define optopt				binkd_optopt
+#define optarg				binkd_optarg
+
 extern int getopt(int argc, char **argv, const char *opts);
 extern void init_getopt(void);
 
