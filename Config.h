8263eccc521e99eeeb8694fb4948510286f90bd4
Pavel Gulchouck <gul@gul.kiev.ua>
2008-01-14 11:42:55 +0000

    Auto increase patchlevel, set 1.0a-510 Fixed bug in protocol logic (partial files send without NR-mode)

--- Config.h
+++ Config.h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 509
+#define PATCHLEVEL 510
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
