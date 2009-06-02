4cc611d29dfae5ce780f7390131a1a9b31822d79
Pavel Gulchouck <gul@gul.kiev.ua>
2009-06-02 17:10:12 +0000

    Auto increase patchlevel, set 1.0a-532 Build binkd for OS/2 with perl support

--- Config.h
+++ Config.h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 531
+#define PATCHLEVEL 532
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
--- mkfls/unix/binkd.spec
+++ mkfls/unix/binkd.spec
@@ -1,6 +1,6 @@
 Summary: Binkd - the binkp daemon
 Name: binkd
-Version: 1.0a.531
+Version: 1.0a.532
 Release: g1
 Copyright: GPL
 Source: %{name}-%{version}.tar.gz
