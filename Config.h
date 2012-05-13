ebfe02e364e4d379633c390b84f957e9d4bd666c
Pavel Gulchouck <gul@gul.kiev.ua>
2012-05-13 17:26:42 +0000

    Auto increase patchlevel, set 1.0a-609 Possibility to specify $passwd for session in on_handshake() perl hook

--- Config.h
+++ Config.h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 0
 #define SUFFIX "a"
-#define PATCHLEVEL 608
+#define PATCHLEVEL 609
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
