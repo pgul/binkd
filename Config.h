a6c91372447294dd30ad124ef6ae4849c77d063c
Roman Trunov <stream@proxyma.ru>
2013-11-08 18:22:34 +0000

    Auto increase patchlevel, set 1.1a-35 Build on MSVC 2000

--- Config.h
+++ Config.h
@@ -61,7 +61,7 @@
 #define MAJOR 1
 #define MINOR 1
 #define SUFFIX "a"
-#define PATCHLEVEL 34
+#define PATCHLEVEL 35
 #define MYVER_S(s) _MYVER_S(s)
 #define _MYVER_S(s) #s
 #define MYVER MYVER_S(MAJOR) "." MYVER_S(MINOR) SUFFIX "-" MYVER_S(PATCHLEVEL)
--- mkfls/unix/binkd.spec
+++ mkfls/unix/binkd.spec
@@ -1,6 +1,6 @@
 Summary: Binkd - the binkp daemon
 Name: binkd
-Version: 1.1a.34
+Version: 1.1a.35
 Release: 1
 License: GPL
 Source: %{name}.tar.gz
